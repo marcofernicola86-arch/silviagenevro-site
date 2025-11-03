8611e0f887a9f030c2f2146be30f53cc2f7c8b49
marcofernicola86-arch <marcofernicola86@gmail.com>
2025-11-03 23:36:54 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,8 @@
 module.exports = function (eleventyConfig) {
-  // Copia pass-through degli asset statici
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("admin");
-
   return {
-    dir: {
-      input: ".",          // <-- prende sia /index.html sia /src/blog/**
-      includes: "layouts", // <-- i tuoi layout stanno in /layouts (a fianco di /src)
-      output: "_site"
-    },
+    dir: { input: ".", includes: "layouts", output: "_site" },
     templateFormats: ["njk", "html", "md"]
   };
 };
